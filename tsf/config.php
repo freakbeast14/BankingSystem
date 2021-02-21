2a46039f1f90c3e07ad7a7846cf762df90bbfe11
freakbeast14 <61834393+freakbeast14@users.noreply.github.com>
2021-02-21 22:29:14 +0530

    Update config.php

--- tsf/config.php
+++ tsf/config.php
@@ -1,6 +1,7 @@
 <?php
 
-	$conn = mysqli_connect('localhost','root','root','bank');
+	$conn = mysqli_connect('sql102.epizy.com','
+ua5u29NtXH','epiz_27985111','epiz_27985111_bank');
 
 	if(!$conn){
 		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
